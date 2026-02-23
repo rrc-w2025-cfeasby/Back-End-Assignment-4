6ee10bf91ed2c55139a0c020ada7e21452904f89
Chris Feasby <cfeasby@academic.rrc.ca>
2026-02-23 13:10:57 -0600

    Change for Scenario A requirements

--- src/api/v1/middleware/authenticate.ts
+++ src/api/v1/middleware/authenticate.ts
@@ -33,7 +33,7 @@ const authenticate = async (
         if (!token) {
             throw new AuthenticationError(
                 "Unauthorized: No token provided",
-                "TOKEN_NOT_FOUND"
+                "MISSING_AUTHORIZATION"
             );
         }
 
