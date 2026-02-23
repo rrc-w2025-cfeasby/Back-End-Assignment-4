10bc401c03b37cf9d21c1570bac5171a9248f5f8
Chris Feasby <cfeasby@academic.rrc.ca>
2026-02-23 11:04:19 -0600

    Update authorize to check userId against the uid

--- src/api/v1/middleware/authorize.ts
+++ src/api/v1/middleware/authorize.ts
@@ -1,10 +1,8 @@
-// External library imports
 import { Request, Response, NextFunction } from "express";
-
-// Internal module imports
 import { AuthorizationOptions } from "../models/authorizationOptions";
 import { MiddlewareFunction } from "../types/expressTypes";
 import { AuthorizationError } from "../errors/errors";
+import { loans } from "../controllers/loanController";
 
 /**
  * Middleware to check if a user is authorized based on their role or UID.
@@ -22,11 +20,15 @@ const isAuthorized = (opts: AuthorizationOptions): MiddlewareFunction => {
     return (req: Request, res: Response, next: NextFunction) => {
         try {
             const { role, uid } = res.locals;
-            const { id } = req.params;
 
             // Allow if the same user is accessing their own data
-            if (opts.allowSameUser && id && uid === id) {
-                return next();
+            if(opts.allowSameUser){
+                const loan_id = Number(req.params.id);
+                const loan = loans.find(loan => loan.id === loan_id);
+
+                if(loan && loan.userId === uid){
+                    return next();
+                }
             }
 
             // If no role exists on the user, throw Forbidden response
