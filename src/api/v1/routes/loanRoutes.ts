747d4fda8386fff957fed8aa725ac7775f7bc73c
Chris Feasby <cfeasby@academic.rrc.ca>
2026-02-23 11:04:48 -0600

    Update to use isAuthorized and the new types of methods for approve and deny

--- src/api/v1/routes/loanRoutes.ts
+++ src/api/v1/routes/loanRoutes.ts
@@ -4,15 +4,37 @@ import {
     getLoanById,
     createLoan,
     updateLoan,
-    deleteLoan   
+    deleteLoan,
+    approveLoan,
+    denyLoan   
 } from "../controllers/loanController";
+import authenticate from "../middleware/authenticate";
+import isAuthorized from "../middleware/authorize";
 
 const router: Router = Router();
 
-router.get("/loans", getLoans);
-router.get("/loans/:id", getLoanById);
-router.post("/loans", createLoan);
-router.put("/loans/:id", updateLoan);
-router.delete("/loans/:id", deleteLoan);
+router.use(authenticate);
+
+router.get("/loans", 
+    isAuthorized({ hasRole: ["admin", "manager"]}),
+    getLoans);
+router.get("/loans/:id", 
+    isAuthorized({ hasRole: ["admin", "manager"], allowSameUser: true}),
+    getLoanById);
+router.post("/loans", 
+    isAuthorized({ hasRole: ["admin", "user"]}),
+    createLoan);
+router.put("/loans/:id", 
+    isAuthorized({ hasRole: ["admin", "user"], allowSameUser: true}),
+    updateLoan);
+router.delete("/loans/:id", 
+    isAuthorized({ hasRole: ["admin"]}),
+    deleteLoan);
+router.post("/:id/approve", 
+    isAuthorized({ hasRole: ["admin", "manager"]}),
+    approveLoan);
+router.post("/:id/deny", 
+    isAuthorized({ hasRole: ["admin", "manager"]}),
+    denyLoan);
 
 export default router;
