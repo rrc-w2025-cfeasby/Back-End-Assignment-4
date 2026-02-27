e79db8b3ec9dc9638445c6edde074f1645438dbb
Chris Feasby <cfeasby@academic.rrc.ca>
2026-02-27 09:19:25 -0600

    Forgot to uncomment a line, so its done. For global error handling to be using my errorHandler routine.

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,18 @@
-import express from "express";
+import express, { Express } from "express";
 import loanRoutes from "./api/v1/routes/loanRoutes";
 import adminRoutes from "./api/v1/routes/adminRoutes";
 import { accessLogger, errorLogger, consoleLogger } from "./api/v1/middleware/logger";
+import errorHandler from "./api/v1/middleware/errorHandler";
 
-const app = express();
+const app: Express = express();
 
-// Logging middleware (should be applied early in the middleware stack)
 if (process.env.NODE_ENV === "production") {
-    // In production, log to files
     app.use(accessLogger);
     app.use(errorLogger);
 } else {
-    // In development, log to console for immediate feedback
     app.use(consoleLogger);
 }
 
-// Body parsing middleware
 app.use(express.json());
 
 // Health Route
@@ -39,7 +36,6 @@ app.get("/", (req, res) => {
     res.send("Hello, World!");
 });
 
-// Global error handling middleware (MUST be applied last)
-//app.use(errorHandler);
+app.use(errorHandler);
 
 export default app;
